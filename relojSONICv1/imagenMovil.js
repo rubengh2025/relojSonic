f2f34e92b113d0a793d7069062f694fbf5021452
rubengh2025 <rubenleiss2731@gmail.com>
2025-04-20 23:27:56 -0300

    Add files via upload
    reloj analogico ambientado en Sonic hecho con codigo html 20/4/2025

---
+++ relojSONICv1/imagenMovil.js
@@ -0,0 +1,27 @@
+﻿let selectedElement = null;
+let mouseX, mouseY, initialX, initialY;
+
+
+document.getElementById('imagen').addEventListener('mousedown', function(e) {
+    selectedElement = e.target;
+    initialX = e.clientX - selectedElement.getBoundingClientRect().left;
+    initialY = e.clientY - selectedElement.getBoundingClientRect().top;
+    
+    document.addEventListener('mousemove', moveElement);
+    document.addEventListener('mouseup', releaseElement);
+});
+
+
+function moveElement(e) {
+    mouseX = e.clientX - initialX;
+    mouseY = e.clientY - initialY;
+    
+    selectedElement.style.left = mouseX + 'px';
+    selectedElement.style.top = mouseY + 'px';
+}
+
+
+function releaseElement() {
+    document.removeEventListener('mousemove', moveElement);
+    document.removeEventListener('mouseup', releaseElement);
+}
